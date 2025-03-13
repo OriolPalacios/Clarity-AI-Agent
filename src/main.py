5460e76da08c13ec007648a3006f2e52a691cbfe
Oriol Palacios <orioloriolpalacios@outlook.com>
2025-03-12 21:50:06 -0500

    chore: starter project schema

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Create a DataFrame with one row and three columns
+df = pd.DataFrame({
+    'A': [1],
+    'B': [2],
+    'C': [3]
+})
+
+# Display the DataFrame
+print(df)
